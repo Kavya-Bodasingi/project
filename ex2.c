8ee2e50e2406f3ae5d385845e1d8e6da4e13204e
Kavya_Bodasingi <kavyabodasingi42@gmail.com>
2023-06-27 16:36:42 +0530

    MY FIRST MODIFIED FILE

--- ex2.c
+++ ex2.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 int main(){
     printf("HII KAVYA WELCOME TO VISUSAL STUDIOS");
-    printf("HELLO RGUKT")
+    printf("HELLO RGUKT");
+    printf("HELLO RGUKT"); 
+    printf("HII KAVYA WELCOME TO VISUSAL STUDIOS");
 }
